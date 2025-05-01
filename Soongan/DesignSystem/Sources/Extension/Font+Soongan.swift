7e2430475fa477f77e09d977e48e4515248dfa6f
Genesis <dmsgktn1685@naver.com>
2025-05-01 18:09:03 +0900

    [Feat] #1 - DesignSystem 모듈에 색상, 폰트, 이미지 구현

---
+++ Soongan/DesignSystem/Sources/Extension/Font+Soongan.swift
@@ -0,0 +1,59 @@
+//
+//  Font+Soongan.swift
+//  Studing
+//
+//  Created by ParkJunHyuk on 5/1/25.
+//
+
+import SwiftUI
+import Resource
+
+extension Font {
+    static var headline24: Font {
+        return ResourceFontFamily.Pretendard.bold.swiftUIFont(size: 24)
+    }
+    
+    static var headline18: Font {
+        return ResourceFontFamily.Pretendard.bold.swiftUIFont(size: 18)
+    }
+    
+    static var headline16: Font {
+        return ResourceFontFamily.Pretendard.bold.swiftUIFont(size: 16)
+    }
+    
+    static var subTitle15: Font {
+        return ResourceFontFamily.Pretendard.bold.swiftUIFont(size: 15)
+    }
+    
+    static var subTitle14: Font {
+        return ResourceFontFamily.Pretendard.bold.swiftUIFont(size: 14)
+    }
+    
+    static var subTitle13: Font {
+        return ResourceFontFamily.Pretendard.bold.swiftUIFont(size: 13)
+    }
+    
+    static var body15: Font {
+        return ResourceFontFamily.Pretendard.regular.swiftUIFont(size: 15)
+    }
+    
+    static var body14: Font {
+        return ResourceFontFamily.Pretendard.regular.swiftUIFont(size: 14)
+    }
+    
+    static var info15: Font {
+        return ResourceFontFamily.Pretendard.regular.swiftUIFont(size: 15)
+    }
+    
+    static var info14: Font {
+        return ResourceFontFamily.Pretendard.regular.swiftUIFont(size: 14)
+    }
+    
+    static var info13: Font {
+        return ResourceFontFamily.Pretendard.regular.swiftUIFont(size: 13)
+    }
+    
+    static var info12: Font {
+        return ResourceFontFamily.Pretendard.regular.swiftUIFont(size: 12)
+    }
+}
