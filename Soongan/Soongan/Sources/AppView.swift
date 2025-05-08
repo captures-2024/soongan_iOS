8701bbc8a246f631308f7691783f488a71b77aaa
Genesis <dmsgktn1685@naver.com>
2025-05-08 20:16:15 +0900

    [Feat] #3 - App 모듈에 앱 진입점,분기점을 위한 AppView & Feature 구현 및 앱 로고 추가

---
+++ Soongan/Soongan/Sources/AppView.swift
@@ -0,0 +1,43 @@
+//
+//  AppView.swift
+//  Soongan
+//
+//  Created by ParkJunHyuk on 5/7/25.
+//  Copyright © 2025 Captures. All rights reserved.
+//
+
+import SwiftUI
+
+import ComposableArchitecture
+import AuthFeature
+
+public struct AppView: View {
+    
+    @Bindable var store: StoreOf<AppFeature>
+    
+    public var body: some View {
+        
+        switch store.authState {
+        case .loggedIn:
+            // TODO: - 로그인된 상태 - 메인 화면 표시
+            EmptyView()
+            
+        case .loggedOut:
+            LoginView(store: store.scope(state: \.login, action: \.login))
+            
+        case .skipped:
+            // TODO: - 둘러보기 - 메인 화면 표시
+            EmptyView()
+        }
+    }
+}
+
+// MARK: - Preview
+
+#Preview {
+    AppView(
+        store: Store(initialState: AppFeature.State()) {
+            AppFeature()
+        }
+    )
+}
