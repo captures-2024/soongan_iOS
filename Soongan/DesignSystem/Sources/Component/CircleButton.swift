aae065a518fb655be232e5fa0f7e83ccd673c45c
Genesis <dmsgktn1685@naver.com>
2025-05-15 16:30:28 +0900

    [Feat] #5 - DesignSystem 모듈에 CircleButton 구현

---
+++ Soongan/DesignSystem/Sources/Component/CircleButton.swift
@@ -0,0 +1,56 @@
+//
+//  CircleButton.swift
+//  DesignSystem
+//
+//  Created by ParkJunHyuk on 5/14/25.
+//
+
+import SwiftUI
+
+public struct CircleButton: View {
+    
+    // MARK: - Property
+    
+    private let type: CircleButtonType
+    private let action: () -> Void
+    
+    // MARK: - Init
+    
+    public init(
+        type: CircleButtonType,
+        action: @escaping () -> Void
+    ) {
+        self.type = type
+        self.action = action
+    }
+    
+    // MARK: - Body
+    
+    public var body: some View {
+        VStack(spacing: 8) {
+            Button(action: {
+                action()
+            }) {
+                ZStack {
+                    Circle()
+                        .fill(Color.white)
+                        .frame(width: 44, height: 44)
+                    
+                    type.image
+//                        .resizable()
+                        .frame(width: type.size.width, height: type.size.height)
+                }
+            }
+            
+            Text(type.title)
+                .font(.regualr12)
+                .foregroundStyle(Color.black100)
+        }
+    }
+}
+
+// MARK: - Preview
+
+#Preview {
+    CircleButton(type: .rightArrow, action: {})
+}
