184f80a4bf425f86b516076863cd7c87f69b39ab
Genesis <dmsgktn1685@naver.com>
2025-06-18 10:15:10 +0900

    [Feat] #13 - Auth 관련한 DTO 및 Endpoint 구현

---
+++ Soongan/Core/CoreNetwork/Sources/Auth/AuthEndpoint.swift
@@ -0,0 +1,65 @@
+//
+//  AuthEndpoint.swift
+//  CoreNetwork
+//
+//  Created by ParkJunHyuk on 6/16/25.
+//
+
+import Alamofire
+
+public enum AuthEndpoint {
+    case postLogin(LoginRequestDTO)
+    case postLogout
+    case patchRefresh(RefreshRequestDTO)
+    case postWithdraw
+}
+
+extension AuthEndpoint: APIEndpoint {
+    public var basePath: BasePath {
+        return .auth
+    }
+    
+    public var path: String {
+        switch self {
+        case .postLogin:
+            return basePath.rawValue + "/login"
+        case .postLogout:
+            return basePath.rawValue + "/logout"
+        case .patchRefresh:
+            return basePath.rawValue + "/refresh"
+        case .postWithdraw:
+            return basePath.rawValue + "/withdraw"
+        }
+    }
+    
+    public var method: Alamofire.HTTPMethod {
+        switch self {
+        case .postLogin, .postLogout, .postWithdraw:
+            return .post
+        case .patchRefresh:
+            return .patch
+        }
+    }
+    
+    public var headerType: HeaderType {
+        switch self {
+        case .postLogin:
+            return .userAgentHeader("IOS")
+        case .patchRefresh:
+            return .defaultHeader
+        case .postLogout, .postWithdraw:
+            return .accessTokenHeader
+        }
+    }
+    
+    public var parameters: (any Encodable)? {
+        switch self {
+        case .postLogin(let dto):
+            return dto
+        case .patchRefresh(let dto):
+            return dto
+        default:
+            return nil
+        }
+    }
+}
