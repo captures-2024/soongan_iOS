2d0332b5cda86cc870bd500b74b24ba14e0c793f
Genesis <dmsgktn1685@naver.com>
2025-07-08 22:44:01 +0900

    [Feat] #15 - 프로필 수정 API 연동 및 이미지 선택 기능 및 사진첩 이미지 선택 기능 추가

--- Soongan/Feature/MypageFeature/Sources/Logic/EditProfileFeature.swift
+++ Soongan/Feature/MypageFeature/Sources/Logic/EditProfileFeature.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 import CoreNetwork
 import DesignSystem
@@ -18,15 +19,27 @@ public struct EditProfileFeature {
     
     @ObservableState
     public struct State: Equatable {
-
+        let baseNickname: String
+        let baseIntroduce: String?
+        var baseProfileImageUrl: String?
+        
         var editButtonState: Bool = false
         var nickname: String = ""
         var introduce: String = ""
+        var userProfileData: Data?
+        
+        var isNicknameButtonEnabled = false
+        var isIntroduceButtonEnabled = false
         
         var nicknameState: TextFieldState = .normal
         var introduceState: TextFieldState = .normal
         
-        public init() {}
+        var isProfileSheetPresented: Bool = false
+        var isPhotosPickerPresented: Bool = false
+        
+        var selectedItem: PhotosPickerItem? = nil
+        var selectedImage: UIImage? = nil
+        var imageData: Data? = nil
     }
 
     // MARK: - Init
@@ -38,9 +51,24 @@ public struct EditProfileFeature {
     public enum Action: BindableAction {
         case binding(BindingAction<State>)
         
-        case editMyProfile
+        case onAppear
+        case isEditButtonEnabledChanged
+        case editMyProfileButtonTapped
         case editMyProfileSuccess(EditMyProfileResponseDTO)
+        
+        case showProfileSheet
+        case baseProfileOptionTapped
+        case selectImageOptionTapped
+        case photoSelectionLoaded(Result<Data?, Error>)
+        
+        case dismissProfileSheet(Bool)
         case backButtonTapped
+        
+        case delegate(DelegateAction)
+
+        public enum DelegateAction {
+            case backButtonTapped
+        }
     }
     
     // MARK: - Body
@@ -50,12 +78,21 @@ public struct EditProfileFeature {
         
         Reduce { state, action in
             switch action {
-            case .editMyProfile:
+            case .onAppear:
+                state.nickname = state.baseNickname
+                
+                if let introduce = state.baseIntroduce {
+                    state.introduce = introduce
+                }
+                
+                return .none
+                
+            case .editMyProfileButtonTapped:
                 let dto = EditMyProfileRequestDTO(
                     nickname: state.nickname,
-                    selfIntroduction: state.introduce,
-                    profileImage: "",
-                    isDefaultProfileImage: false
+                    selfIntroduction: state.introduce.isEmpty ? nil : state.introduce,
+                    profileImage: state.imageData,
+                    isDefaultProfileImage: (state.imageData != nil || state.baseProfileImageUrl != nil) ? false : true
                 )
                 
                 return .run { send in
@@ -69,10 +106,104 @@ public struct EditProfileFeature {
                     }
                 }
                 
-            case .editMyProfileSuccess(let response):
-
-                return .none
+            case .editMyProfileSuccess(_):
+                return .send(.delegate(.backButtonTapped))
             
+            case .baseProfileOptionTapped:
+                state.selectedItem = nil
+                state.selectedImage = nil
+                state.imageData = nil
+                state.baseProfileImageUrl = nil
+                state.isProfileSheetPresented = false
+                return .none
+                
+            case .selectImageOptionTapped:
+                state.isProfileSheetPresented = false
+                
+                return .run { send in
+                    try await Task.sleep(for: .seconds(0.5))
+                    await send(.set(\.isPhotosPickerPresented, true))
+                }
+                
+            case .showProfileSheet:
+                state.isProfileSheetPresented = true
+                return .none
+                
+            case .dismissProfileSheet(let isPresented):
+                state.isProfileSheetPresented = isPresented
+                return .none
+                
+            case .binding(\.nickname):
+                switch state.nickname.nicknameValidationState {
+                case .empty, .tooShort, .tooLong:
+                    state.nicknameState = .normal
+                    state.isNicknameButtonEnabled = false
+                    
+                case .invalidCharacters:
+                    state.nicknameState = .error(message: .specialCharacter)
+                    state.isNicknameButtonEnabled = false
+                    
+                case .valid:
+                    state.nicknameState = .possible
+                    state.isNicknameButtonEnabled = true
+                    
+                case .onlyConsonantsOrVowels:
+                    state.nicknameState = .error(message: .onlyConsonantsOrVowels)
+                    state.isNicknameButtonEnabled = false
+                }
+                
+                return .send(.isEditButtonEnabledChanged)
+                
+            case .binding(\.introduce):
+                if state.introduce.count > 0 && state.introduce.count <= 25 {
+                    state.introduceState = .possible
+                    state.isIntroduceButtonEnabled = true
+                } else {
+                    state.introduceState = .normal
+                    state.isIntroduceButtonEnabled = false
+                }
+                
+                return .send(.isEditButtonEnabledChanged)
+                
+            case .binding(\.selectedItem):
+                guard let selectedItem = state.selectedItem else {
+                    return .none
+                }
+                
+                return .run { send in
+                    let result = await Result {
+                        try await selectedItem.loadTransferable(type: Data.self)
+                    }
+                    await send(.photoSelectionLoaded(result))
+                }
+            
+            case .photoSelectionLoaded(.success(let data)):
+                guard let data, let uiImage = UIImage(data: data) else {
+                    return .none
+                }
+                
+                state.selectedImage = uiImage
+                state.baseProfileImageUrl = nil
+                
+                // 이미지 압축 로직
+                if let compressedData = uiImage.jpegData(compressionQuality: 0.2) {
+                    print("이미지 용량", compressedData)
+                    state.imageData = compressedData
+                } else {
+                    state.imageData = data
+                }
+                
+                return .none
+                
+            case .photoSelectionLoaded(.failure(let error)):
+                print("Image Loading Failed: \(error.localizedDescription)")
+                return .none
+                
+            case .isEditButtonEnabledChanged:
+                state.editButtonState = state.isNicknameButtonEnabled && state.isIntroduceButtonEnabled
+                
+                return .none
+                
             case .backButtonTapped:
                 
                 return .none
--- Soongan/Feature/MypageFeature/Sources/View/EditProfileView.swift
+++ Soongan/Feature/MypageFeature/Sources/View/EditProfileView.swift
@@ -6,11 +6,13 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 import DesignSystem
 import Shared
 
 import ComposableArchitecture
+import Kingfisher
 
 public struct EditProfileView: View {
     
@@ -25,61 +27,45 @@ public struct EditProfileView: View {
         store: StoreOf<EditProfileFeature>
     ) {
         self.store = store
+        
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = UIColor(DesignSystem.Color.soonganBG)
+        appearance.shadowColor = .clear
+
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
     }
     
     // MARK: - Body
     
     public var body: some View {
         ZStack {
-            Color.clear
-                .contentShape(Rectangle())
+            DesignSystem.Color.soonganBG
+                .ignoresSafeArea()
                 .onTapGesture {
                     UIApplication.shared.dismissKeyboard()
                 }
             
             VStack {
-                Button(action: {
-                    
-                }) {
-                    ZStack(alignment: .bottomTrailing) {
-                        Image.editProfile
-                        
-                        Image.addProfile
-                    }
-                }
-                .padding(.top, 42)
-                .padding(.bottom, 40)
+                profileImageSection()
+                    .padding(.top, 42)
+                    .padding(.bottom, 40)
                 
-                VStack {
-                    CustomTextFieldView(
-                        type: .changeNickname,
-                        text: $store.nickname,
-                        isFocused: $isNicknameFieldFocused,
-                        state: $store.nicknameState
-                    )
-                    .padding(.bottom, 48)
-                    
-                    CustomTextFieldView(
-                        type: .introduce,
-                        text: $store.introduce,
-                        isFocused: $isIntroductionFocused,
-                        state: $store.introduceState
-                    )
-                }
-                .padding(.horizontal, 20)
+                inputFieldsSection()
+                    .padding(.horizontal, 20)
                 
                 Spacer()
                 
                 CustomBottomButton(type: .editComplete, isEnable: $store.editButtonState) {
-                    store.send(.editMyProfile)
+                    store.send(.editMyProfileButtonTapped)
                 }
                 .padding(.horizontal, 136)
                 .padding(.bottom, 93)
             }
             .frame(maxHeight: .infinity)
-            .background(DesignSystem.Color.soonganBG)
-            .background(InteractivePopGestureEnabler())
         }
+        .background(InteractivePopGestureEnabler())
         .navigationBarBackButtonHidden(true)
         .toolbar {
             ToolbarItem(placement: .navigationBarLeading) {
@@ -94,12 +80,93 @@ public struct EditProfileView: View {
                 .padding(.top, 16)
             }
         }
+        .onAppear {
+            store.send(.onAppear)
+        }
+        .photosPicker(
+            isPresented: $store.isPhotosPickerPresented,
+            selection: $store.selectedItem,
+            matching: .images
+        )
+        .sheet(
+            isPresented: $store.isProfileSheetPresented.sending(\.dismissProfileSheet)
+        ) {
+            CustomSheetView<EditProfileType>(type: .selectProfile(isBaseProfile: (store.imageData == nil && store.baseProfileImageUrl == nil))) {
+                option in
+                switch option {
+                case .baseProfile:
+                    store.send(.baseProfileOptionTapped)
+                case .selectImage:
+                    store.send(.selectImageOptionTapped)
+                }
+            }
+        }
+    }
+}
+
+// MARK: - Private Extension View
+
+private extension EditProfileView {
+    func profileImageSection() -> some View {
+        VStack {
+            Button(action: {
+                store.send(.showProfileSheet)
+            }) {
+                if let imageUrl = store.baseProfileImageUrl {
+                    ZStack(alignment: .bottomTrailing) {
+                        KFImage(URL(string: imageUrl)!)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 180, height: 180)
+                            .clipShape(Circle())
+                        
+                        Image.addProfile
+                    }
+                } else if let image = store.selectedImage {
+                    ZStack(alignment: .bottomTrailing) {
+                        Image(uiImage: image)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 180, height: 180)
+                            .clipShape(Circle())
+                        
+                        Image.addProfile
+                    }
+                } else {
+                    ZStack(alignment: .bottomTrailing) {
+                        Image.editProfile
+                        Image.addProfile
+                    }
+                }
+            }
+        }
+    }
+    
+    func inputFieldsSection() -> some View {
+        VStack(spacing: 0) {
+            CustomTextFieldView(
+                type: .changeNickname,
+                text: $store.nickname,
+                isFocused: $isNicknameFieldFocused,
+                state: $store.nicknameState
+            )
+            .padding(.bottom, 36)
+            
+            CustomTextFieldView(
+                type: .introduce,
+                text: $store.introduce,
+                isFocused: $isIntroductionFocused,
+                state: $store.introduceState
+            )
+        }
     }
 }
 
 #Preview {
-    EditProfileView(store: Store(initialState: EditProfileFeature.State()) {
-            EditProfileFeature()
-        }
-    )
+    NavigationStack {
+        EditProfileView(store: Store(initialState: EditProfileFeature.State(baseNickname: "", baseIntroduce: "", baseProfileImageUrl: nil)) {
+                EditProfileFeature()
+            }
+        )
+    }
 }
