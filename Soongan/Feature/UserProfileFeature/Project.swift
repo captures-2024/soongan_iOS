f00f8efac89348089d61ac79e563ba5c7b05131c
Genesis <dmsgktn1685@naver.com>
2025-07-22 10:38:56 +0900

    [Feat] #17 - 유저 프로필을 볼 수 있는 UserProfileFeature 모듈 구현

---
+++ Soongan/Feature/UserProfileFeature/Project.swift
@@ -0,0 +1,30 @@
+import ProjectDescription
+
+let project = Project(
+    name: "UserProfileFeature",
+    packages: [
+      .package(
+        url: "https://github.com/pointfreeco/swift-composable-architecture.git",
+        from: "1.17.0"
+      ),
+    ],
+    targets: [
+        .target(
+            name: "UserProfileFeature",
+            destinations: .iOS,
+            product: .staticFramework,
+            bundleId: "com.captures.UserProfileFeature.Soongan",
+            deploymentTargets: .iOS("17.0"),
+            infoPlist: .default,
+            sources: ["Sources/**"],
+            resources: [],
+            dependencies: [
+                .external(name: "ComposableArchitecture"),
+                .project(target: "DesignSystem", path: "../../DesignSystem"),
+                .project(target: "Shared", path: "../../Shared"),
+                .project(target: "CoreNetwork", path: "../../Core/CoreNetwork"),
+                .project(target: "AppDependencies", path: "../../AppDependencies"),
+            ]
+        )
+    ]
+)
---
+++ Soongan/Feature/UserProfileFeature/Sources/View/UserProfileView.swift
@@ -0,0 +1,61 @@
+//
+//  UserProfileView.swift
+//  UserProfileFeature
+//
+//  Created by ParkJunHyuk on 5/15/25.
+//
+
+import SwiftUI
+
+import DesignSystem
+import Shared
+
+import ComposableArchitecture
+import Kingfisher
+
+public struct UserProfileView: View {
+    
+    @Bindable var store: StoreOf<UserProfileFeature>
+    
+    // MARK: - Init
+    
+    public init(
+        store: StoreOf<UserProfileFeature>
+    ) {
+        self.store = store
+    }
+    
+    // MARK: - Body
+    
+    public var body: some View {
+        VStack(spacing: 0) {
+            ProfileHeaderView(
+                userName: store.userName ?? "정보없음",
+                userIntroduction: store.userIntroduction ?? "정보없음",
+            ) {
+                if let url = store.userProfileImageUrl {
+                    KFImage(URL(string: url)!)
+                        .resizable()
+                } else {
+                    Image(systemName: "person.circle.fill")
+                        .resizable()
+                }
+            }
+            
+            if store.leftContestImageList.isEmpty {
+                EmptyView()
+            } else {
+                ImageGridView(
+                    leftImageList: store.leftContestImageList,
+                    rightImageList: store.rightContestImageList,
+                    onImageTap: { _ in
+                        
+                })
+                .padding(.bottom, 50)
+            }
+        }
+        .onAppear {
+            store.send(.onAppear)
+        }
+    }
+}
