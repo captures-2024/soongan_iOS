b4a10f1bb7b6e395cfbd4a5a1110010ecc7ae108
Genesis <dmsgktn1685@naver.com>
2025-07-22 10:49:08 +0900

    [Feat] #17 - 유저 프로필을 보여주는 UserProfileHeaderView 구현

---
+++ Soongan/DesignSystem/Sources/Component/UserProfileHeaderView.swift
@@ -0,0 +1,62 @@
+//
+//  UserProfileHeaderView.swift
+//  DesignSystem
+//
+//  Created by ParkJunHyuk on 7/17/25.
+//
+
+import SwiftUI
+
+public struct ProfileHeaderView<ProfileImage: View>: View {
+    
+    // ViewBuilder를 통해 외부에서 프로필 이미지 뷰를 주입받습니다.
+    private let profileImage: ProfileImage
+    
+    private let userName: String
+    private let userIntroduction: String
+    
+    // MARK: - Init
+    
+    public init(
+        userName: String,
+        userIntroduction: String,
+        @ViewBuilder profileImage: () -> ProfileImage
+    ) {
+        self.userName = userName
+        self.userIntroduction = userIntroduction
+        self.profileImage = profileImage()
+    }
+    
+    // MARK: - Body
+    
+    public var body: some View {
+        HStack(alignment: .center, spacing: 18) {
+            profileImage
+                .scaledToFill()
+                .frame(width: 60, height: 60)
+                .clipShape(Circle())
+            
+            profileHeaderSection()
+                .frame(maxWidth: .infinity, alignment: .leading)
+        }
+    }
+}
+
+// MARK: - Private Extension View
+
+private extension ProfileHeaderView {
+    func profileHeaderSection() -> some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text(userName)
+                .font(.medium16)
+            
+            Text(userIntroduction)
+                .font(.regular12)
+        }
+        .foregroundStyle(Color.black100)
+    }
+}
+
+//#Preview {
+//    UserProfileHeaderView()
+//}
